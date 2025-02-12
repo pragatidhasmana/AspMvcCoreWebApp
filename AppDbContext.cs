03031c0b242288e2449b02c28588105e7249d6ac
Pragati D <pragati.dhasmana99@gmail.com>
2025-02-12 17:18:37 +0530

    commit

---
+++ AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AspMvcCoreWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AspMvcCoreWebApp
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }        
+        
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace AspMvcCoreWebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
